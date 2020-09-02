6d8fb74a19bbdbe703f1ec183dce9856d693f328
Danick Takam <otis.takam@datnek.be>
2020-09-02 15:30:25 +0200

    migration des données

--- ng-datnek-angular-map-address/projects/ng-datnek-angular-map-address/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-datnek-angular-map-address
- */
-
-export * from './lib/ng-datnek-angular-map-address.service';
-export * from './lib/ng-datnek-angular-map-address.component';
-export * from './lib/ng-datnek-angular-map-address.module';
